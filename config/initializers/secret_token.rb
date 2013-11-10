6952cb2b5ff1531324ce49ecda8fbe65d20e71fc
Timur Kaplya <timur.kaplya@gmail.com>
2013-11-10 18:29:10 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Magicpoint::Application.config.secret_key_base = '02c59b2c58f6adb5ab42779345bd55a440d3b08220f6d10f77357eb00643bb84c036745baa1f26965d6ae30cc3c0464d00ed6b5475e3c0e8f52724ba80686ab7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Magicpoint::Application.config.session_store :cookie_store, key: '_magicpoint_session'
