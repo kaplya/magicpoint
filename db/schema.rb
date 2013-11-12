96bf78c21455abbcf15e30566a51fdb3eedffa26
Timur Kaplya <timur.kaplya@gmail.com>
2013-11-12 22:17:07 +0400

    can add images

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131110144431) do
+ActiveRecord::Schema.define(version: 20131112173336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
